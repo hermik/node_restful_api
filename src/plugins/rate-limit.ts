86d99504fd26be20181839d2c95bbcbfe487b6fe
hermik <mhermanowicz@gmail.com>
2026-09-06 11:27:47 +0200

    Change default limits to 200

--- src/plugins/rate-limit.ts
+++ src/plugins/rate-limit.ts
@@ -4,7 +4,7 @@ import type { FastifyInstance } from "fastify";
 
 export default fp(async function rateLimitPlugin(fastify: FastifyInstance) {
   await fastify.register(rateLimit, {
-    max: 100,
+    max: 200,
     timeWindow: "1 minute",
     errorResponseBuilder: (_request, context) => ({
       statusCode: context.statusCode,
